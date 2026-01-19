87bce28af9903910e406dfd966dfa8b6b4e94796
maths <matheusalan.dev@gmail.com>
2026-01-19 14:19:18 -0300

    Implementando camada controller para entidade Category

--- src/category/controller/category.controller.ts
+++ src/category/controller/category.controller.ts
@@ -1,6 +1,30 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe } from "@nestjs/common";
+import { CategoryService } from "../service/categoria.service";
+import { Category } from "../entity/category.entity";
 
 @Controller()
 export class CategoryController {
+
+    constructor(private readonly categoryService: CategoryService) { }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Category[]> {
+        return this.categoryService.findAll();
+    }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Category> {
+        return this.categoryService.findById(id);
+    }
+
+    @Get('/name/:name')
+    @HttpCode(HttpStatus.OK)
+    findAllByName(@Param('name') name: string): Promise<Category[]> {
+        return this.categoryService.findAllByName(name);
+    }
+
     
+     
 }
